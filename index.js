8094a3a447291ee8887a457558fd7279630ec42a
parharx <66666579+parharx@users.noreply.github.com>
2021-07-17 10:14:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector('.hambar');
+const navList = document.querySelector('.nav-list');
+const lists = document.querySelectorAll('.nav-list li');
+
+hamburger.addEventListener('click', () => {
+    navList.classList.toggle("open");
+    // document.querySelector('.fas').style.color = "#03FF50";
+
+    // links.forEach
+});
